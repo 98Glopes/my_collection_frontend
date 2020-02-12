7f0ee1bfcb41d800c9d5cc47795bd3ece2e2477b
98Glopes <gabriel.lopes@outlook.com>
2020-02-11 22:43:00 -0300

    add single page aplication concept

--- js/request.js
+++ js/request.js
@@ -23,5 +23,31 @@ function makeRequest(url , callback) {
     httpRequest.send();
   }
 
+  function makePOSTRequest(url , callback, data) {
+    if (window.XMLHttpRequest) { // Mozilla, Safari, ...
+      httpRequest = new XMLHttpRequest();
+    } else if (window.ActiveXObject) { // IE
+      try {
+        httpRequest = new ActiveXObject("Msxml2.XMLHTTP");
+      } 
+      catch (e) {
+        try {
+          httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+        } 
+        catch (e) {}
+      }
+    }
+
+    if (!httpRequest) {
+      alert('Giving up :( Cannot create an XMLHTTP instance');
+      return false;
+    }
+    httpRequest.onreadystatechange = callback;
+    httpRequest.open('POST' , url);
+    httpRequest.setRequestHeader('Content-type', 'application/json')
+    httpRequest.setRequestHeader('Access-Control-Allow-Origin', '*');
+    httpRequest.send(JSON.stringify(data));
+  }
+
 
   
--- js/template.js
+++ js/template.js
@@ -1,4 +1,4 @@
-function render_comics(comic){
+function renderHome(comic){
 var html = `
             <div class="col-md-4">
             <h2> ${ comic.name }</h2>
@@ -19,16 +19,16 @@ var content = document.getElementById("content");
 
 function distributeHTML(element, index, array){
 
-    content.insertAdjacentHTML("afterbegin", render_comics(element))
+    content.insertAdjacentHTML("afterbegin", renderHome(element))
 
 }
 
 //data.forEach(distributeHTML);
 
 bodyTag = document.getElementsByTagName('body');
-bodyTag.onload = makeRequest('https://mycollectionapi.herokuapp.com/comic_books/', handleCallback);
+bodyTag.onload = makeRequest('https://mycollectionapi.herokuapp.com/comic_books/', loadHome);
 
-function handleCallback() {
+function loadHome() {
  if (httpRequest.readyState === 4) {
    if (httpRequest.status === 200) {
        var data = JSON.parse(httpRequest.response);
@@ -37,4 +37,94 @@ function handleCallback() {
      alert('There was a problem with the request.');
    }
  }
+}
+
+function loadRegister(){
+
+  var html = `<form class="form-horizontal" id="registerForm" onsubmit="return false">
+  <fieldset>
+  
+  <!-- Form Name -->
+  <legend>Register a new comic book</legend>
+  
+  <!-- Text input-->
+  <div class="form-group">
+    <label class="col-md-12 control-label" for="textinput">Title</label>  
+    <div class="col-md-12">
+    <input id="textinput" name="name" type="text" placeholder="Title" class="form-control input-md" required="">
+      
+    </div>
+  </div>
+  
+  <!-- Text input-->
+  <div class="form-group">
+    <label class="col-md-12 control-label" for="autor">Autor</label>  
+    <div class="col-md-12">
+    <input id="autor" name="autor" type="text" placeholder="Autor" class="form-control input-md">
+      
+    </div>
+  </div>
+  
+  <!-- Text input-->
+  <div class="form-group">
+    <label class="col-md-12 control-label" for="publisher">Publisher</label>  
+    <div class="col-md-12">
+    <input id="publisher" name="publisher" type="text" placeholder="Publisher" class="form-control input-md" required="">
+      
+    </div>
+  </div>
+  
+  <!-- Textarea -->
+  <div class="form-group">
+    <label class="col-md-12 control-label" for="description">Description</label>
+    <div class="col-md-12">                     
+      <textarea class="form-control" id="description" name="description"></textarea>
+    </div>
+  </div>
+
+  </fieldset>
+  </form>
+    <div class="col-md-12">
+    <button type='button' onclick="registerNew()" class="btn btn-primary">Save</button>
+  </div>
+  <div class="form-group">
+
+
+</div>`; 
+
+  var content = document.getElementById("content");
+  content.innerHTML = html
+
+}
+
+document.getElementById('register').addEventListener("click", loadRegister);
+
+function getForm(formId) {
+  var elements = document.getElementById(formId).elements;
+
+  var obj ={};
+  for(var i = 1 ; i < elements.length ; i++){
+      var item = elements.item(i);
+      console.log(item.name);
+      obj[item.name] = item.value;
+  }
+
+  return obj;
+}
+function registerNewCallback(){
+  if (httpRequest.readyState === 4) {
+    console.log(httpRequest.status)
+    if (httpRequest.status === 201) {
+      console.log(httpRequest.response);
+      } else {
+      alert('There was a problem with the request.');
+    }
+  }
+}
+function registerNew(){
+
+  var data = getForm("registerForm");
+  console.log(data);
+  makePOSTRequest("http://0.0.0.0:5000/comic_books/", registerNewCallback, data);
+
 }
